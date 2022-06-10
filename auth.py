fd4e8bf505718fcc1bc7c61671e72571fafb3ab0
Caleb Hartman <38409536+Callama@users.noreply.github.com>
2022-06-10 19:43:24 -0400

    testing getting enviro. varibles

--- auth.py
+++ auth.py
@@ -11,8 +11,9 @@ def verifyCredentials(session, email, password):
 def getDBCredsandConnect(session,type="prod"):
     if type == "beta":
         api_key = os.environ['heroku_api_key']
+
     if type == "prod":
-        s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
-        api_key = s3['api_key']
+        api_key = os.environ['api_key']
+
     req = requests.get("https://api.heroku.com/apps/callamadev/config-vars",headers={"Authorization": f"Bearer {api_key}", "Accept": "application/vnd.heroku+json; version=3"})
     return req.content
