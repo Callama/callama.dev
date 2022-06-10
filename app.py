84dc8b9de41b3491d2cba120b377bcf172f36016
Caleb Hartman <38409536+Callama@users.noreply.github.com>
2022-06-10 15:21:08 -0400

    enable subdomain matching in flask

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 
 # in the Procfile, we have app:app. this is looking for the file, app, with the webserver var, app. it must match.
-app = Flask(__name__,template_folder="misc/templates")
+app = Flask(__name__,template_folder="misc/templates",subdomain_matching=True)
 app.config["DEBUG"] = True
 
 @app.route("/")
