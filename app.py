89cbca10ea3748545b521f1a3b591b1c75e21e68
Caleb Hartman <38409536+Callama@users.noreply.github.com>
2022-06-15 18:57:02 -0400

    stop circular importing

--- app.py
+++ app.py
@@ -12,9 +12,34 @@ import json
 # in the Procfile, we have app:app. this is looking for the file, app, with the webserver var, app. it must match.
 app = Flask(__name__,template_folder="misc/templates",subdomain_matching=True)
 app.config["DEBUG"] = True
+
 import auth
 
-dbConn = auth.getDBCredsandConnect(type=version)
+def getDBCredsandConnect(type):
+    """ 
+    Get DB creds and connect to the databse
+
+    :param type: str, "beta" or "prod" to dertimine how to gather DB creds
+
+    :return: db connection object
+    """
+    # when it is being ran on an enviroment that is not heroku
+    if type == "beta":
+        api_key = os.environ['heroku_api_key']
+
+    if type == "prod":
+       api_key = os.environ['api_key']
+
+    req = requests.get("https://api.heroku.com/apps/callamadev/config-vars",headers={"Authorization": f"Bearer {api_key}", "Accept": "application/vnd.heroku+json; version=3"})
+    creds = req.json()
+    # remove this from the data 
+    creds.pop("api_key")
+    # connect to the database using the creds we just got
+    dbConn = psycopg2.connect(**creds)
+    return dbConn
+
+global dbConn
+dbConn = getDBCredsandConnect(type=version)
 
 
 
