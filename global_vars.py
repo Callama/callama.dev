c18061d72319f2c9c81c329df93660a40fa631dc
Caleb Hartman <38409536+Callama@users.noreply.github.com>
2022-06-17 23:04:46 -0400

    change version type

--- global_vars.py
+++ global_vars.py
@@ -31,7 +31,7 @@ def getDBCredsandConnect(type="prod"):
 
 def __init__():
     global version
-    version="beta"
+    version="prod"
 
     global dbConn
     dbConn = getDBCredsandConnect(type=version)
